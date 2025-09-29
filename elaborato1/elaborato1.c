a1c97e8169569037d014f2699a21e45b099cbd85
mxttex <matteofaccetta@gmail.com>
2025-09-29 14:12:37 +0200

    sistemato

--- elaborato1/elaborato1.c
+++ elaborato1/elaborato1.c
@@ -25,20 +25,20 @@ void main() {
     */
 
     printf("TYPE: char NAME: \"c\" VALUE: %c MIN: %d MAX: %d BYTE: %d\n", c, CHAR_MIN, CHAR_MAX, sizeof(c));
+    printf("TYPE: signed char NAME: \"sc\" VALUE: %c MIN: %d MAX: %d BYTE: %d\n", sc, SCHAR_MIN, SCHAR_MAX, sizeof(sc));
+    printf("TYPE: unsigned char NAME: \"uc\" VALUE: %c MIN: %d MAX: %d BYTE: %d\n", uc, 0, UCHAR_MAX, sizeof(uc));
     printf("TYPE: int NAME: \"i\" VALUE: %d MIN: %d MAX: %d BYTE: %d\n", i, INT_MIN, INT_MAX, sizeof(i));
-    printf("TYPE: float NAME: \"f\" VALUE: %f MIN: %f MAX: %e BYTE: %d\n", f, FLT_MIN, FLT_MAX, sizeof(f));
-    printf("TYPE: double NAME: \"d\" VALUE: %f MIN: %f MAX: %e BYTE: %d\n", d, DBL_MIN, DBL_MAX, sizeof(d));
-    printf("TYPE: char NAME: \"sc\" VALUE: %c MIN: %d MAX: %d BYTE: %d\n", sc, SCHAR_MIN, SCHAR_MAX, sizeof(sc));
-    printf("TYPE: char NAME: \"uc\" VALUE: %c MIN: %d MAX: %d BYTE: %d\n", uc, SCHAR_MIN, UCHAR_MAX, sizeof(uc));
+    printf("TYPE: float NAME: \"f\" VALUE: %f MIN: %e MAX: %e BYTE: %d\n", f, FLT_MIN, FLT_MAX, sizeof(f));
+    printf("TYPE: double NAME: \"d\" VALUE: %f MIN: %e MAX: %e BYTE: %d\n", d, DBL_MIN, DBL_MAX, sizeof(d));
     printf("TYPE: int NAME: \"si\" VALUE: %d MIN: %d MAX: %d BYTE: %d\n", si, INT_MIN, INT_MAX, sizeof(si));
     printf("TYPE: unsigned int NAME: \"ui\" VALUE: %u MIN: %d MAX: %d BYTE: %d\n", ui, INT_MIN, UINT_MAX, sizeof(ui));
     printf("TYPE: short int NAME: \"shi\" VALUE: %d MIN: %d MAX: %d BYTE: %d\n", shi, SHRT_MIN, SHRT_MAX, sizeof(shi));
     printf("TYPE: short signed int NAME: \"ssi\" VALUE: %d MIN: %d MAX: %d BYTE: %d\n", ssi, SHRT_MIN, SHRT_MAX, sizeof(ssi));
-    printf("TYPE: short unsigned int NAME: \"usi\" VALUE: %u MIN: %d MAX: %d BYTE: %d\n", usi, SHRT_MIN, USHRT_MAX, sizeof(usi));
+    printf("TYPE: short unsigned int NAME: \"usi\" VALUE: %d MIN: %d MAX: %d BYTE: %d\n", usi, SHRT_MIN, USHRT_MAX, sizeof(usi));
     printf("TYPE: long int NAME: \"li\" VALUE: %d MIN: %d MAX: %d BYTE: %d\n", li, LONG_MIN, LONG_MAX, sizeof(li));
     printf("TYPE: signed long int NAME: \"sli\" VALUE: %d MIN: %d MAX: %d BYTE: %d\n", sli, LONG_MIN, LONG_MAX, sizeof(sli));
-    printf("TYPE: unsigned long int NAME: \"uli\" VALUE: %d MIN: %d MAX: %d BYTE: %d\n", uli, LONG_MIN, ULONG_MAX, sizeof(uli));
-    printf("TYPE: double NAME: \"ld\" VALUE: %f MIN: %f MAX: %e BYTE: %d", ld, LDBL_MIN, LDBL_MAX, sizeof(ld));
+    printf("TYPE: unsigned long int NAME: \"uli\" VALUE: %d MIN: %d MAX: %d BYTE: %d\n", uli, 0UL, ULONG_MAX, sizeof(uli));
+    printf("TYPE: long double NAME: \"ld\" VALUE: %e MIN: %e MAX: %e BYTE: %d", ld, LDBL_MIN, LDBL_MAX, sizeof(ld));
 
     return 0;
 }
