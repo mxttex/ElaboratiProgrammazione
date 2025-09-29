0355fc716c100c2f0ac893c9db82847a04865f10
matteo.faccetta.stud <matteo.faccetta.stud@ispascalcomandini.it>
2025-09-29 18:03:46 +0200

    sistemati gli ultimi formattatori , continua a dare warning ma perche vs non considera il C90

--- elaborato1/elaborato1.c
+++ elaborato1/elaborato1.c
@@ -20,25 +20,25 @@ void main() {
     unsigned long int uli = 468793575748;
     long double ld = 7.568;
 
-    /*stampa a console per ogni variabile, seguendo il formato: 
+    /*stampa a console per ogni variabile, seguendo il formato:
     TYPE: char NAME: c VALUE: x MIN: -128 MAX: 127 BYTE: 1
     */
 
-    printf("TYPE: char NAME: \"c\" VALUE: %c MIN: %d MAX: %d BYTE: %d\n", c, CHAR_MIN, CHAR_MAX, sizeof(c));
-    printf("TYPE: signed char NAME: \"sc\" VALUE: %c MIN: %d MAX: %d BYTE: %d\n", sc, SCHAR_MIN, SCHAR_MAX, sizeof(sc));
-    printf("TYPE: unsigned char NAME: \"uc\" VALUE: %c MIN: %d MAX: %d BYTE: %d\n", uc, 0, UCHAR_MAX, sizeof(uc));
-    printf("TYPE: int NAME: \"i\" VALUE: %d MIN: %d MAX: %d BYTE: %d\n", i, INT_MIN, INT_MAX, sizeof(i));
-    printf("TYPE: float NAME: \"f\" VALUE: %f MIN: %e MAX: %e BYTE: %d\n", f, FLT_MIN, FLT_MAX, sizeof(f));
-    printf("TYPE: double NAME: \"d\" VALUE: %f MIN: %e MAX: %e BYTE: %d\n", d, DBL_MIN, DBL_MAX, sizeof(d));
-    printf("TYPE: int NAME: \"si\" VALUE: %d MIN: %d MAX: %d BYTE: %d\n", si, INT_MIN, INT_MAX, sizeof(si));
-    printf("TYPE: unsigned int NAME: \"ui\" VALUE: %u MIN: %d MAX: %d BYTE: %d\n", ui, INT_MIN, UINT_MAX, sizeof(ui));
-    printf("TYPE: short int NAME: \"shi\" VALUE: %d MIN: %d MAX: %d BYTE: %d\n", shi, SHRT_MIN, SHRT_MAX, sizeof(shi));
-    printf("TYPE: short signed int NAME: \"ssi\" VALUE: %d MIN: %d MAX: %d BYTE: %d\n", ssi, SHRT_MIN, SHRT_MAX, sizeof(ssi));
-    printf("TYPE: short unsigned int NAME: \"usi\" VALUE: %d MIN: %d MAX: %d BYTE: %d\n", usi, SHRT_MIN, USHRT_MAX, sizeof(usi));
-    printf("TYPE: long int NAME: \"li\" VALUE: %d MIN: %d MAX: %d BYTE: %d\n", li, LONG_MIN, LONG_MAX, sizeof(li));
-    printf("TYPE: signed long int NAME: \"sli\" VALUE: %d MIN: %d MAX: %d BYTE: %d\n", sli, LONG_MIN, LONG_MAX, sizeof(sli));
-    printf("TYPE: unsigned long int NAME: \"uli\" VALUE: %d MIN: %d MAX: %d BYTE: %d\n", uli, 0UL, ULONG_MAX, sizeof(uli));
-    printf("TYPE: long double NAME: \"ld\" VALUE: %e MIN: %e MAX: %e BYTE: %d", ld, LDBL_MIN, LDBL_MAX, sizeof(ld));
+    printf("TYPE: char NAME: \"c\" VALUE: %c MIN: %d MAX: %d BYTE: %u\n", c, CHAR_MIN, CHAR_MAX, sizeof(c));
+    printf("TYPE: signed char NAME: \"sc\" VALUE: %c MIN: %d MAX: %d BYTE: %u\n", sc, SCHAR_MIN, SCHAR_MAX, sizeof(sc));
+    printf("TYPE: unsigned char NAME: \"uc\" VALUE: %c MIN: %d MAX: %d BYTE: %u\n", uc, 0, UCHAR_MAX, sizeof(uc));
+    printf("TYPE: int NAME: \"i\" VALUE: %d MIN: %d MAX: %d BYTE: %u\n", i, INT_MIN, INT_MAX, sizeof(i));
+    printf("TYPE: signed int NAME: \"si\" VALUE: %d MIN: %d MAX: %d BYTE: %u\n", si, INT_MIN, INT_MAX, sizeof(si));
+    printf("TYPE: unsigned int NAME: \"ui\" VALUE: %u MIN: %d MAX: %d BYTE: %u\n", ui, 0, UINT_MAX, sizeof(ui));
+    printf("TYPE: short int NAME: \"shi\" VALUE: %d MIN: %d MAX: %d BYTE: %u\n", shi, SHRT_MIN, SHRT_MAX, sizeof(shi));
+    printf("TYPE: short signed int NAME: \"ssi\" VALUE: %d MIN: %d MAX: %d BYTE: %u\n", ssi, SHRT_MIN, SHRT_MAX, sizeof(ssi));
+    printf("TYPE: short unsigned int NAME: \"usi\" VALUE: %d MIN: %d MAX: %d BYTE: %u\n", usi, 0, USHRT_MAX, sizeof(usi));
+    printf("TYPE: long int NAME: \"li\" VALUE: %d MIN: %d MAX: %d BYTE: %u\n", li, LONG_MIN, LONG_MAX, sizeof(li));
+    printf("TYPE: signed long int NAME: \"sli\" VALUE: %d MIN: %d MAX: %d BYTE: %u\n", sli, LONG_MIN, LONG_MAX, sizeof(sli));
+    printf("TYPE: unsigned long int NAME: \"uli\" VALUE: %d MIN: %d MAX: %d BYTE: %u\n", uli, 0UL, ULONG_MAX, sizeof(uli));
+    printf("TYPE: float NAME: \"f\" VALUE: %f MIN: %e MAX: %e BYTE: %u\n", f, FLT_MIN, FLT_MAX, sizeof(f));
+    printf("TYPE: double NAME: \"d\" VALUE: %f MIN: %e MAX: %e BYTE: %u\n", d, DBL_MIN, DBL_MAX, sizeof(d));
+    printf("TYPE: long double NAME: \"ld\" VALUE: %e MIN: %e MAX: %e BYTE: %u", ld, LDBL_MIN, LDBL_MAX, sizeof(ld));
 
     return 0;
 }
