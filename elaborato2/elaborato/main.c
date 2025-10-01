5fc6c1ff0c7c3a70e306c1c9fbac59b7b34a4673
matteo.faccetta.stud <matteo.faccetta.stud@ispascalcomandini.it>
2025-10-01 18:42:48 +0200

    inserito scheletro elaborato

--- elaborato2/elaborato/main.c
+++ elaborato2/elaborato/main.c
@@ -1,7 +1,11 @@
 #include<stdio.h>
+#include<math.h>
+#include "quadratic_eq.h" //con "" invece di <>, perche' il file e' nella stessa cartella del file main.c
 
 void main() {
-	printf("forza napoli");
+	/*Implementare le seguenti macro per calcolare alcune proprietà di una
+		funzione di secondo grado nel campo reale :
+	f(x) = ax2 + bx + c*/
 
 	return 0;
 }  
---
+++ elaborato2/elaborato/quadratic_eq.h
@@ -0,0 +1,5 @@
+#define NUM_OF_ROOTS(a, b, c)
+#define ROOT1(a, b, c)
+#define ROOT2(a, b, c)
+#define EXTREME_POINT(a, b, c) //dando per scontato che a != 0, il vertice della parabola sara' sempre un punto di massimo o minimo
+#define MAXIMUM(a, b, c)
