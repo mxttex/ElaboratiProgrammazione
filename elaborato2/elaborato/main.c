b0397a23ae039c18f9ab8ae4c0af8ea0eb0451de
matteo.faccetta.stud <matteo.faccetta.stud@ispascalcomandini.it>
2025-10-02 12:22:04 +0200

    scritte le prime tre macro + marco d'appoggio DELTA con relativo test per main

--- elaborato2/elaborato/main.c
+++ elaborato2/elaborato/main.c
@@ -5,7 +5,37 @@
 void main() {
 	/*Implementare le seguenti macro per calcolare alcune proprietà di una
 		funzione di secondo grado nel campo reale :
-	f(x) = ax2 + bx + c*/
+	f(x) = ax^2 + bx + c*/
+
+	//dichiarazione variabili
+	float a = 0;
+	float b = 0;
+	float c = 0;
+
+	//valori della funzione presi in input
+	printf("Tenendo in considerazione f(x) = ax^2 + bx + c\nInserire il valore di A\n");
+	scanf("%f", &a);
+	while (a == 0) {
+		printf("A non puo' essere 0, reinserire il valore di A\n");
+		scanf("%f", &a);
+	}
+	printf("Inserire il valore di B\n");
+	scanf("%f", &b);
+	printf("Inserire il valore di C\n");
+	scanf("%f", &c);
+
+	//switch che in base al numero di soluzione stampa quante soluzioni ha la funzione 
+	switch (NUM_OF_ROOTS(a, b, c)) {
+	case 0:
+		printf("L'equazione non ha soluzioni");
+		break;
+	case 1:
+		printf("L'equazione si risolve in x = %f", ROOT1(a, b, c));
+		break;
+	default:
+		printf("L'equazione si risolve in x1 = %f e in x2 = %f", ROOT1(a, b, c), ROOT2(a, b, c));
+		break;
+	};
 
 	return 0;
-}  
+}
--- elaborato2/elaborato/quadratic_eq.h
+++ elaborato2/elaborato/quadratic_eq.h
@@ -1,5 +1,10 @@
-#define NUM_OF_ROOTS(a, b, c)
-#define ROOT1(a, b, c)
-#define ROOT2(a, b, c)
+//controllo il numero di soluzioni in base al delta
+#define NUM_OF_ROOTS(a, b, c) DELTA (a, b, c) > 0 ? 2 : DELTA (a, b, c) == 0 ? 1 : 0
+#define ROOT1(a, b, c) ((-b + sqrt(DELTA(a, b, c))) / (2 * a))
+#define ROOT2(a, b, c) ((-b - sqrt(DELTA(a, b, c))) / (2 * a))
 #define EXTREME_POINT(a, b, c) //dando per scontato che a != 0, il vertice della parabola sara' sempre un punto di massimo o minimo
 #define MAXIMUM(a, b, c)
+
+
+//macro che aiuta la leggibilita' del codice
+#define DELTA(a, b, c) (((b) * (b)) - (4 * (a) * (c)))
