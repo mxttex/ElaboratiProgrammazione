2e0383157d89c6642abf6621c5adacb92aeaef27
Matteo Faccetta <113626542+mxttex@users.noreply.github.com>
2025-10-08 14:03:06 +0200

    Update esercizio_triangoli.h

--- .esercizi_lab/esercizi_lab/esercizio_triangoli.h
+++ .esercizi_lab/esercizi_lab/esercizio_triangoli.h
@@ -1,7 +1,7 @@
 void esercizio_triangoli() {
 	//la difficilissima selezione if
 	int alfa, beta, gamma; // ampiezza angoli , in gradi
-	printf(" Che tipo di triangolo abbiamo ? \n Inserisci l ’ ampiezza dei tre angoli in gradi : ");
+	printf(" Che tipo di triangolo abbiamo ? \n Inserisci l â€™ ampiezza dei tre angoli in gradi : ");
 	scanf("%d %d %d", &alfa, &beta, &gamma);
 	/* Dobbiamo c o n t r o l l a r e :
 	- se un triangolo
@@ -13,14 +13,14 @@ void esercizio_triangoli() {
 		printf("non e' un triangolo");
 	else {
 		if (alfa == 90 || beta == 90 || gamma == 90) {
-			printf("Il triangolo è rettangolo\n");
+			printf("Il triangolo Ã¨ rettangolo\n");
 		}
 		if (alfa == 60)
-			printf("Il triangolo è equilatero\n");
-		if (alfa == beta || beta == gamma || gamma == alfa)
-			printf("Il triangolo è isoscele\n");
+			printf("Il triangolo Ã¨ equilatero\n");
+		else if (alfa == beta || beta == gamma || gamma == alfa)
+			printf("Il triangolo Ã¨ isoscele\n");
 		else
-			printf("Il triangolo è scaleno");
+			printf("Il triangolo Ã¨ scaleno");
 	}
 
 	// printf (" I tre angoli non fanno parte di un t rian gol o \ n ") ;
