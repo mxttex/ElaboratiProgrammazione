981f1a8e19400eac243f8dba4f8b7fd97cc3b991
mxttex <matteofaccetta@gmail.com>
2025-10-28 15:54:11 +0100

    creato scheletro elaborato 6

---
+++ elaborato6/game.c
@@ -0,0 +1,46 @@
+#include"game.h"
+void setup_game(int height, int width,
+		   struct position ball_pos, struct position ball_dir,
+		   struct position pad1_pos, struct position pad2_pos, int pad_len) {
+	// Implementation of game setup
+}
+void move_ball(void) {
+	// Implementation of ball movement
+}
+void move_pad1_up(void) {
+	// Implementation of moving pad1 up
+}
+void move_pad1_down(void) {
+	// Implementation of moving pad1 down
+}
+void move_pad2_up(void) {
+	// Implementation of moving pad2 up
+}
+void move_pad2_down(void) {
+	// Implementation of moving pad2 down
+}
+struct position get_pad1_pos(void) {
+	// Implementation of getting pad1 position
+}
+//get_pad1_len(void) {
+//	// Implementation of getting pad length
+//}
+//get_pad2_len(void) {
+//
+//}
+struct position get_pad2_pos(void) {
+	// implementation of getting pad2 position
+}
+struct position get_ball_pos(void) {
+	// Implementation of getting ball position
+}	
+
+unsigned int get_pad_len(void) {
+	// Implementation of getting pad length
+}
+unsigned int get_pad1_score(void) {
+	// Implementation of getting pad1 score
+}
+unsigned int get_pad2_score(void) {
+	// Implementation of getting pad2 score
+}
