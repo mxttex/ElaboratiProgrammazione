68747f8b40f5120f8465a6e2a765069d4a6ba763
mxttex <matteofaccetta@gmail.com>
2025-12-06 12:52:50 +0100

    sistemato metodo creazione snake

--- elaborato9/snake.c
+++ elaborato9/snake.c
@@ -5,9 +5,14 @@
 typedef struct snake snake;
 typedef struct body body;
 typedef struct position position;
+#define GO_UP(i, rows) ((i) - 1 + (rows) % rows)  
+#define GO_DOWN(i, rows) ((i) + 1 + (rows) % rows)  
+#define GO_LEFT(j, cols) ((j) - 1 + (cols) % cols)  
+#define GO_RIGHT(j, cols) ((j) + 1 + (cols) % cols)  
+
 
 static body* body_create(body* next, body* prev, position p) {
-	body* b = (body*)malloc(sizeof(body));
+	body* b = (body*)calloc(1, sizeof(body));
 	if (b == NULL)
 		return NULL;
 	b->next = next;
@@ -16,7 +21,7 @@ static body* body_create(body* next, body* prev, position p) {
 	return b;
 }
 struct snake* snake_create(unsigned int rows, unsigned int cols) {
-	snake* s = (snake *)malloc(sizeof(snake));
+	snake* s = (snake *)calloc(1, sizeof(snake));
 	if (s == NULL)
 		return NULL;
 	s->cols = cols;
@@ -25,24 +30,37 @@ struct snake* snake_create(unsigned int rows, unsigned int cols) {
 	position starting_pos;
 	starting_pos.i = rows / 2;
 	starting_pos.j = cols / 2;
-	body_create(NULL, NULL, starting_pos);
+	s->body = body_create(NULL, NULL, starting_pos);
 
 	return s;
 }
 
 void snake_kill(struct snake* s) {
+	body* current = s->body;
+	while (current != NULL) {
+		body* next = current->next;
+		free(current);
+		current = next;
+	}
 	
 	free(s);
 }
 
 struct position snake_head(struct snake* s) {
 	struct position p;
+	if (s->body != NULL) {
+		p = s->body->pos;
+	}
 	return p;
 
 }
 
 struct position snake_body(struct snake* s, unsigned int i) {
 	struct position p;
+	if (s != NULL) {
+		body* b = s->body;
+		p = b[i].pos;
+	}
 	return p;
 }
 
@@ -51,6 +69,7 @@ int snake_knotted(struct snake* s) {
 }
 
 void snake_move(struct snake* s, enum direction dir) {
+	
 }
 
 void snake_reverse(struct snake* s) {
